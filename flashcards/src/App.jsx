bada0947bef243d43a801334f95006a205e87e97
UjwalShakhakarmi <ujwalshakha@gmail.com>
2023-12-12 15:04:15 +0545

    whole

---
+++ flashcards/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <FlashCards que={questions} />
+    </div>
+  );
+}
+
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+
+function FlashCards({ que }) {
+  const [selectedId, setSelectedId] = useState(null);
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
+  return (
+    <div className="flashcards">
+      {questions.map((que) => (
+        <div
+          key={que.id}
+          className={selectedId === que.id ? "selected " : ""}
+          onClick={() => handleClick(que.id)}
+        >
+          {selectedId === que.id ? que.answer : que.question}
+        </div>
+      ))}
+    </div>
+  );
+}
